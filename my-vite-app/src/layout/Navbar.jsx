0937054a5c6a595bd47e9a584e907ae7c96619b2
thepriyanshu22-oss <theamritesh22@gmail.com>
2026-07-31 01:57:44 +0530

    Fix import path for Button component in Navbar

--- my-vite-app/src/layout/Navbar.jsx
+++ my-vite-app/src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/Button";
+import { Button } from "../Components/Button";
 import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
 
