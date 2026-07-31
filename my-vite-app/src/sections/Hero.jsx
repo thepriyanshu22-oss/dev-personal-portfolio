46171593a45d1d17824a5256a7c06c97aa862960
thepriyanshu22-oss <theamritesh22@gmail.com>
2026-07-31 23:06:30 +0530

    Update Hero.jsx

--- my-vite-app/src/sections/Hero.jsx
+++ my-vite-app/src/sections/Hero.jsx
@@ -36,7 +36,7 @@ export const Hero = () => {
       {/* Bg */}
       <div className="absolute inset-0">
         <img
-          src="/hero-bg.jpg"
+          src="/"D:\dev-personal-protfolio\my-vite-app\public\Project\profile-image.jpeg"
           alt="Hero image"
           className="w-full h-full object-cover opacity-40"
         />
